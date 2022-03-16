4be69448eed3b6744f540fa67170ae6729021d15
cassius de avila <cassius.avila@edu.pucrs.br>
2022-03-16 20:01:58 -0300

    fix: ajuste na chamada da função

--- max-val-v2.js
+++ max-val-v2.js
@@ -5,16 +5,19 @@ function max(arg1, arg2) {
   return arg1 > arg2 ? arg1 : arg2;
 }
 
-function maxVal(array, init, end, at) {
+function maxVal(array) {
+  iterator++
   if (end - init < 2) {
     return max(array[init], array[end]);
   }
 
   const middle = Math.floor((init + end) / 2);
   const firstValue = maxVal(array, init, middle);
-  console.log(middle, end);
   const secondValue = maxVal(array, middle + 1, end);
+  console.log(firstValue, secondValue);
   return max(firstValue, secondValue);
 }
 
-console.log(maxVal(arrayGenerator(20), 0, 20));
+const arrayLength = 20
+
+console.log(maxVal(arrayGenerator(arrayLength), 0, arrayLength - 1));
